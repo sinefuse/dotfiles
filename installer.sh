21be655e3bc77586db93ca901b379725e111fb4c
sinefuse <19891857+sinefuse@users.noreply.github.com>
2020-11-11 21:00:09 +0100

    Slight changes to installer.sh

--- installer.sh
+++ installer.sh
@@ -3,7 +3,8 @@
 
 # Basically a more or less guided thing to install stuff.
 # This is an early version, so be warned. lol
-echo Welcome to the installer script. This will guide you through installing all the necessary things to have your nonsense just work... hopefully.
+echo "Welcome to the installer script. This will guide you through installing all the necessary things to have your nonsense just work... hopefully."
+echo "Please note that this script is tailored for use with Fedora 33."
 read -rsp $'Press any key to continue.\n' -n1 key
 echo "Switching to home directory."
 cd ~
@@ -17,17 +18,17 @@ echo "Enabling a copr repository..."
 sudo dnf copr enable mhdahmad/workstation
 
 # Install developer group packages for the sake of having that stuff
+# ...do I really need to, though? lol
+# I'll add this later, it's not super important anyways.
 
 # Install all the dependencies
 echo "Installing a bunch of needed packages..."
 sudo dnf install gnome-tweaks util-linux-user htop maim xdotool xclip mpd ncmpcpp neofetch sakura mpv obs-studio stow terminus-fonts zsh discord -y
 
-
-
 # stow the shit because stow is pretty fuckin neat tbh
 cd dotfiles
 stow sakura zsh
-# todo find a way to ignore installer.sh as stow shits itself if i try "stow *" lol
+# todo find a way to ignore installer.sh as stow shits itself if i try "stow *"
 # grab maimfuse
 echo "Grabbing maimfuse..."
 cd ~
