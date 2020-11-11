4dcb70ff0f6a9d9cb770abe1266127bfeb8e5b23
sinefuse <19891857+sinefuse@users.noreply.github.com>
2020-11-11 20:57:08 +0100

    Initial version of installer.sh complete

--- installer.sh
+++ installer.sh
@@ -2,21 +2,47 @@
 # Installer script for dotfiles among other things
 
 # Basically a more or less guided thing to install stuff.
-# Right now, there isn't really anything in here except for a few snippets that I am gonna need.
+# This is an early version, so be warned. lol
 echo Welcome to the installer script. This will guide you through installing all the necessary things to have your nonsense just work... hopefully.
 read -rsp $'Press any key to continue.\n' -n1 key
-echo Key pressed, you did thing.
-
+echo "Switching to home directory."
+cd ~
 # I'll work on this a bit more but outside of a VM that I currently use. Atom keeps shooting itself in the foot lol
 # However, here's a list of the things that this script will do for me:
 # Install rpmfusion, first and foremost
-
+echo "Installing rpmfusion..."
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 # Install some copr repositories
-# mhdahmad/workstation
+echo "Enabling a copr repository..."
+sudo dnf copr enable mhdahmad/workstation
 
 # Install developer group packages for the sake of having that stuff
 
-# Check whether Cinnamon is installed, just in case.
-
 # Install all the dependencies
-# sudo dnf install gnome-tweaks util-linux-user
+echo "Installing a bunch of needed packages..."
+sudo dnf install gnome-tweaks util-linux-user htop maim xdotool xclip mpd ncmpcpp neofetch sakura mpv obs-studio stow terminus-fonts zsh discord -y
+
+
+
+# stow the shit because stow is pretty fuckin neat tbh
+cd dotfiles
+stow sakura zsh
+# todo find a way to ignore installer.sh as stow shits itself if i try "stow *" lol
+# grab maimfuse
+echo "Grabbing maimfuse..."
+cd ~
+git clone https://github.com/sinefuse/maimfuse
+
+# get maimfuse working
+echo "maimfuse grabbed, installing..."
+cd maimfuse
+chmod +x maimfuse
+sudo ln -s ~/maimfuse/maimfuse /usr/local/bin/maimfuse
+
+# change shell into zsh
+echo "Changing shell into zsh..."
+chsh -s /bin/zsh
+
+# gg ez
+echo "Done!"
+read -rsp $'Please log out and back in to have everything working.' -n1 key
